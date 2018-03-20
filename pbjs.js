ed1c308f9623cefa74ef685c0dd3ef334bd4c719
swim1295 <swim1295@naver.com>
2018-03-21 01:29:32 +0900

    swim1295

--- pbjs.js
+++ pbjs.js
@@ -1,10 +1,20 @@
+document.getElementById('savebtn').onclick = function() {
+    add_row();
+}
 
 
-
-
-function myFunction() {
-    var node = document.createElement("tr");
-    var textnode = document.createTextNode("Water");
-    node.appendChild(textnode);
-    document.getElementById("myList").appendChild(node);
+function add_row() {
+table = document.getElementById("table");
+tr = document.createElement('tr');
+td1 = document.getElementById("td1");
+td2 = document.getElementById("td2");
+td3 = document.getElementById("td3");
+td1.innerText = document.getElementById('ln').value;
+td2.innerText = document.getElementById('fn').value;
+td3.innerText = document.getElementById('pn').value;
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+tr.appendChild(td4);
+table.appendChild(tr);
 }
